09becf6a3effe9ddb853a51b00a7ec0a6aff6865
Olivier Refalo <orefalo@yahoo.com>
2018-02-27 01:41:24 +0200

    fix kafka org.apache.kafka.clients.producer.internals.ErrorLoggingCallback issue

--- local_producer.sh
+++ local_producer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kafka-console-producer --broker-list kafka+ssl://kafka.docker.ssl:9094 --topic test --producer.config ./local_client_ssl.properties
+echo "TEST" | kafka-console-producer --broker-list kafka+ssl://kafka.docker.ssl:9094 --topic test --producer.config ./local_client_ssl.properties
