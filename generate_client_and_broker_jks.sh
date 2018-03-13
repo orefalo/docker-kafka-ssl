4a046ba1f7761d69973dad2ff230dbb8cc298d63
Olivier Refalo <orefalo@yahoo.com>
2018-03-13 13:09:07 -0500

    fixes

--- generate_client_and_broker_jks.sh
+++ generate_client_and_broker_jks.sh
@@ -47,7 +47,7 @@ echo -e "${GREEN}Generating cert & key for the kafka Client...${NC}"
 keytool -keystore $CLIENT_KEYSTORE_JKS -alias client -validity $VALIDITY -genkey -storepass $PASSWORD -keypass $PASSWORD  -dname "CN=$CLIENT_HOSTNAME, OU=None, O=Qv, L=Miami, S=Miami, C=US" -keyalg RSA -keysize $KEYLEN
 
 echo -e "${GREEN}Generate a top level server CA to stamp client certificates${NC}"
-openssl req -new -x509 -keyout $SERVER_CA_KEY -out $SERVER_CA_CERT -days $VALIDITY -passout pass:$PASSWORD -subj "/C=US/S=Miami/L=Miami/O=Hw/OU=None/CN=$SERVER_HOSTNAME"
+openssl req -new -newkey rsa:$KEYLEN -x509 -keyout $SERVER_CA_KEY -out $SERVER_CA_CERT -days $VALIDITY -passout pass:$PASSWORD -subj "/C=US/S=Miami/L=Miami/O=Hw/OU=None/CN=$SERVER_HOSTNAME"
 
 # IMPORTANT: Kafka 2 ways SSL only works with ONE CA ROOT!!
 #echo -e "${GREEN}Generate a top level client CA to stamp server certificates${NC}"
